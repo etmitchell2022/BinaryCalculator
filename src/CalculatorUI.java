3bb548348f7593a85c44b1472e757002be84a520
Foster98 <hgfoster@bsu.edu>
2020-09-08 13:25:09 -0400

    UI first draft

---
+++ src/CalculatorUI.java
@@ -0,0 +1,172 @@
+import javax.swing.*;
+import javax.swing.plaf.FontUIResource;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalculatorUI extends JFrame implements ActionListener {
+
+    Calculator Calculator = new Calculator();
+    JLabel displayLabel;
+
+    public CalculatorUI() {
+
+        super("Binary Calculator");
+
+        //bigger fonts
+        UIManager.put("Label.font", new FontUIResource(new Font("Dialog", Font.PLAIN, 15)));
+        UIManager.put("Button.font", new FontUIResource(new Font("Dialog", Font.BOLD, 15)));
+
+        JPanel panel = new JPanel();
+
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        panel.setLayout(gridBagLayout);
+        setContentPane(panel);
+
+        displayLabel = new JLabel("");
+
+        var displayLabelConstraints = new GridBagConstraints(0, 0, 2, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(20, 20, 20, 20), 0, 0);
+        panel.add(displayLabel, displayLabelConstraints);
+
+
+        JButton button0 = new JButton("0");
+        var button0constraints = new GridBagConstraints(1, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
+        button0.addActionListener(e -> {
+            Calculator.addToMainNumber("0");
+            displayLabel.setText(Calculator.getMainNumber());
+        });
+        panel.add(button0, button0constraints);
+
+
+
+        JButton button1 = new JButton("1");
+        var button1constraints = new GridBagConstraints(0, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
+        button1.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                Calculator.addToMainNumber("1");
+                displayLabel.setText(Calculator.getMainNumber());
+            }
+        });
+        panel.add(button1, button1constraints);
+
+
+        JButton squaredButton = new JButton("x^2");
+        var squaredButtonConstraints = new GridBagConstraints(1, 2, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
+        squaredButton.addActionListener(this);
+        panel.add(squaredButton, squaredButtonConstraints);
+
+        JButton squarerootButton = new JButton("√");
+        var squarerootButtonConstraints = new GridBagConstraints(1, 3, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
+        squarerootButton.addActionListener(this);
+        panel.add(squarerootButton, squarerootButtonConstraints);
+
+
+        JButton plusButton = new JButton("+");
+        var plusButtonConstraints = new GridBagConstraints(2, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
+        plusButton.addActionListener(this);
+        panel.add(plusButton, plusButtonConstraints);
+
+
+        JButton minusButton = new JButton("-");
+        var minusButtonConstraints = new GridBagConstraints(2, 2, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
+        minusButton.addActionListener(this);
+        panel.add(minusButton, minusButtonConstraints);
+
+
+        JButton multiplicationButton = new JButton("x");
+        var multiplicationButtonConstraints = new GridBagConstraints(2, 3, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
+        multiplicationButton.addActionListener(this);
+        panel.add(multiplicationButton, multiplicationButtonConstraints);
+
+
+        JButton divisionButton = new JButton("÷");
+        var divisionButtonConstraints = new GridBagConstraints(2, 4, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
+        divisionButton.addActionListener(this);
+        panel.add(divisionButton, divisionButtonConstraints);
+
+
+        JButton equalButton = new JButton("=");
+        var equalButtonConstraints = new GridBagConstraints(0, 2, 1, 2, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
+        equalButton.addActionListener(this);
+        panel.add(equalButton, equalButtonConstraints);
+
+
+        JButton clearButton = new JButton("C");
+        var clearButtonConstraints = new GridBagConstraints(0, 4, 2, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
+        clearButton.addActionListener(this);
+        panel.add(clearButton, clearButtonConstraints);
+
+
+
+        setPreferredSize(new Dimension(600, 600));
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pack();
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Running Binary Calculator");
+        new CalculatorUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        String text = displayLabel.getText();
+        String prevText = Calculator.getPrevNumber();
+        if (text!=null) {
+
+            if (text.contains("R")) {
+
+                text = text.substring(0, text.indexOf("R"));
+            }
+        }
+        if(prevText != null) {
+
+            if (prevText.contains("R")) {
+
+                Calculator.setPrevNumber(Calculator.getPrevNumber().substring(0, prevText.indexOf("R")));
+            }
+        }
+        if (((JButton) e.getSource()).getText().equals("=")) {
+
+            if(Calculator.getOperation().equals("+")){
+
+                Calculator.addNumber(text);
+                displayLabel.setText(Calculator.getMainNumber());
+            }else if(Calculator.getOperation().equals("-")){
+
+                Calculator.subtractNumber(text);
+                displayLabel.setText(Calculator.getMainNumber());
+            }else if(Calculator.getOperation().equals("x")){
+
+                Calculator.multiplyNumber(text);
+                displayLabel.setText(Calculator.getMainNumber());
+            }else if(Calculator.getOperation().equals("÷")){
+
+                Calculator.divideNumber(text);
+                displayLabel.setText(Calculator.getMainNumber());
+
+                Calculator.resetMainNumber();
+            }
+        }
+        else if(((JButton) e.getSource()).getText().equals("C")) {
+
+            Calculator.resetMainNumber();
+            Calculator.resetPreviousNumber();
+
+            displayLabel.setText("0");
+        }
+        else{
+
+            Calculator.setOperation(((JButton) e.getSource()).getText());
+            Calculator.storeAndResetMainNumber();
+
+            displayLabel.setText(Calculator.getMainNumber());
+            displayLabel.setText(Calculator.operation);
+        }
+    }
+}
