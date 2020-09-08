18060fbd856e59f769dda4a13779d866ac9703de
Evan Mitchell <etmitchell@bsu.edu>
2020-09-08 19:26:20 -0400

    Changed tests to public so they all pass

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
     }
 }
--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -1,36 +1,40 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
-class CalculatorTest {
+public class CalculatorTest {
     @Test
-    void testAddNumber() {
+    public void testAddNumber() {
         Calculator calculator = new Calculator();
         calculator.setPrevNumber("1");
         String actual = calculator.addNumber("1");
-        Assertions.assertEquals("10", actual);
+        Assert.assertEquals("10", actual);
     }
 
     @Test
-    void testSubtractNumber() {
+    public void testSubtractNumber() {
         Calculator calculator = new Calculator();
         calculator.setPrevNumber("1");
         String actual = calculator.subtractNumber("1");
-        Assertions.assertEquals("0", actual);
+        Assert.assertEquals("0", actual);
     }
 
     @Test
-    void testMultiplyNumber() {
+    public void testMultiplyNumber() {
         Calculator calculator = new Calculator();
         calculator.setPrevNumber("10");
         String actual = calculator.multiplyNumber("10");
-        Assertions.assertEquals("100", actual);
+        Assert.assertEquals("100", actual);
     }
 
     @Test
-    void testDivideNumber() {
+    public void testDivideNumber() {
         Calculator calculator = new Calculator();
         calculator.setPrevNumber("100");
         String actual = calculator.divideNumber("10");
-        Assertions.assertEquals("10", actual);
+        Assert.assertEquals("10", actual);
     }
+//    @Test
+//    void test(){
+//        Assert.assertEquals();
+//    }
 }
